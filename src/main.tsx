7f815301323eca9cfaafd42e2b3bbd13bd697f99
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-01-29 15:32:45 -0300

    :wrench: Alterando rotas

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./main/App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cadastro from "./main/pages/Cadastro";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </Router>
   </React.StrictMode>,
